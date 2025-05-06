76a20437b764600f7f107f7d43b3a0ba2ba0cc15
John Stone <john.stone@crowdstrike.com>
2025-05-06 11:21:28 -0400

    fix compiler errors

--- functions/job_history/main.go
+++ functions/job_history/main.go
@@ -164,7 +164,7 @@ func newFalconClient(ctx context.Context, token string) (*client.CrowdStrikeAPIS
 }
 
 func newSearchClient(fc *client.CrowdStrikeAPISpecification) searchc.SearchC {
-	return searchc.NewClient(fc.SavedSearches, host, logger)
+	return searchc.NewClient(fc.FoundryLogscale, host, logger)
 }
 
 func newStorageClient(fc *client.CrowdStrikeAPISpecification, token string) storagec.StorageC {
