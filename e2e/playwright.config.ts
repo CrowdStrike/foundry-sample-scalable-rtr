7e813daf58a18fbce531b1dd598fc60d79103a88
Matt Raible <matt.raible@crowdstrike.com>
2026-02-11 23:38:11 -0700

    Remove hardcoded workers to use Playwright default

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: isCI,
   retries: isCI ? 2 : 0,
-  workers: isCI ? 1 : 4,
   reporter: 'list',
 
   timeout: isCI ? 120000 : 60000,
