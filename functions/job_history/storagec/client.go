b14b833aa6f42276b81534a0616c760b22ab0b02
Matt Raible <matt.raible@crowdstrike.com>
2026-08-10 09:18:56 -0600

    Fix compilation with gofalcon 0.22.0 pagination type changes
    Total changed from *int32 to int32, Offset changed from string to int32.

--- functions/job_history/storagec/client.go
+++ functions/job_history/storagec/client.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strconv"
 	"strings"
 	"sync"
 
@@ -247,10 +246,8 @@ func (f *Client) Search(ctx context.Context, req SearchObjectsRequest) (SearchOb
 
 	sor := SearchObjectsResponse{}
 	if pagination := payload.Meta.Pagination; pagination != nil {
-		if pagination.Total != nil {
-			sor.Total = int(*pagination.Total)
-		}
-		sor.Offset, _ = strconv.Atoi(pagination.Offset)
+		sor.Total = int(pagination.Total)
+		sor.Offset = int(pagination.Offset)
 	}
 	res := payload.Resources
 	if len(res) == 0 {
